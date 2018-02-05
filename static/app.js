016ad8be85fd327d80e5559041d2685c97e03e82
Gabriel SULLICE <gabriel@sullice.com>
2018-02-05 23:01:43 +0100

    Build client helper

--- static/app.js
+++ static/app.js
@@ -1,25 +1,92 @@
 document.addEventListener("DOMContentLoaded", e => {
-  const headers = {"headers": {"x-push-request": "data.[].relationships.uid.links.self"}}
-  const logResource = resource => {
-    const td = text => {
-      const td = document.createElement("td")
-      td.appendChild(document.createTextNode(text))
-      return td
-    }
-    const tr = document.createElement("tr")
-    tr.appendChild(td(resource.type))
-    tr.appendChild(td(resource.id))
-    document.getElementById("resource-log").appendChild(tr)
+  const f = (data, path) => {
+    return extract(data, path).catch(console.log)
   }
-  fetch('/jsonapi/node/article', headers)
-    .then(data => data.json())
-    .then(json => json.data.map(node => {
-      logResource(node)
-      return node.relationships.uid.links.self
-    }))
-    .then(links => links.map(link => (new URL(link)).pathname))
-    .then(paths => Promise.all(paths.map(path => fetch(path)
-      .then(data => data.json())
-    )))
-    .then(users => users.map(user => user.data).map(logResource))
+
+  const g = (link, path, fetchOptions) => {
+    return fetch(link, fetchOptions)
+      .then(response => {
+        return new Promise((resolve, reject) => {
+          if (response.ok) {
+            resolve(response.json())
+          } else {
+            reject(response)
+          }
+        })
+      })
+      .then(data => {
+        return f(data, path)
+      })
+  }
+
+  const h = (links, {path, children}, buildOptions = true) => {
+    const fetchOptions = buildOptions ? buildFetchOptions({path, children}) : {}
+    return links.reduce((acc, link) => {
+      return children
+        ? children.reduce((acc, child) => {
+          return acc.concat(
+            g(link, path, fetchOptions).then(innerLinks => h(innerLinks, child, false))
+          );
+        }, [])
+        : g(link, path, fetchOptions)
+    }, [])
+  }
+
+  Promise.all(h(['/jsonapi/node/article'], {
+    path: "data.[].relationships.uid.links.self", children: [
+      {path: "data.links.self"},
+    ],
+  }))
 });
+
+function extract(data, path) {
+  logDocument(data)
+  extractors = {
+    "data.[].relationships.uid.links.self": data => {
+      return data.data.map(item => {
+        return url2Path(item.relationships.uid.links.self)
+      })
+    }
+  }
+  return extractors.hasOwnProperty(path)
+    ? Promise.resolve(extractors[path](data))
+    : Promise.reject(`The path "${path}" could not be found.`)
+}
+
+function buildFetchOptions(tree) {
+  const buildHeaders = (paths, {path, children}) => {
+    return (children && children.length !== 0)
+      ? children.reduce(buildHeaders, paths.concat(path))
+      : paths.concat(path);
+  }
+  return {
+    headers: {
+      "x-push-request": buildHeaders([], tree).join('; ')
+    },
+  }
+}
+
+function url2Path(link) {
+  return (new URL(link)).pathname
+}
+
+function logDocument(doc) {
+  if (Array.isArray(doc.data)) {
+    doc.data.forEach(logResource)
+  }
+  else {
+    logResource(doc.data)
+  }
+}
+
+function logResource(resource) {
+  const td = text => {
+    const td = document.createElement("td")
+    td.appendChild(document.createTextNode(text))
+    return td
+  }
+  const tr = document.createElement("tr")
+  tr.appendChild(td(resource.type))
+  tr.appendChild(td(resource.id))
+  document.getElementById("resource-log").appendChild(tr)
+}
