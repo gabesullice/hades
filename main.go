fdde515b4c771203d24f3fd0c9c9dc0bb4b588c7
Gabriel SULLICE <gabriel@sullice.com>
2018-03-08 13:25:04 +0100

    Tons

--- main.go
+++ main.go
@@ -10,16 +10,56 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
-	"regexp"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/gabesullice/jq"
+	"github.com/satori/go.uuid"
 )
 
 var (
-	reArray = regexp.MustCompile(`^\s*\[\s*(\d+)(\s*:\s*(\d+))?\s*]\s*$`)
+	requestPushers = RequestPushers{pushers: make(map[string]*RequestPusher)}
 )
+
+type RequestPushers struct {
+	sync.RWMutex
+	pushers map[string]*RequestPusher
+}
+
+func (r *RequestPushers) add(id string, p *RequestPusher) {
+	r.Lock()
+	r.pushers[id] = p
+	r.Unlock()
+}
+
+func (r *RequestPushers) get(id string) (*RequestPusher, bool) {
+	r.RLock()
+	p, ok := r.pushers[id]
+	r.RUnlock()
+	return p, ok
+}
+
+func (r *RequestPushers) remove(id string) {
+	r.Lock()
+	delete(r.pushers, id)
+	r.Unlock()
+}
+
+type RequestPusher struct {
+	sync.WaitGroup
+	p http.Pusher
+}
+
+func (r *RequestPusher) Push(target string, opts *http.PushOptions) {
+	log.Printf("Pushing: %s", target)
+	r.Add(1)
+	if err := r.p.Push(target, opts); err != nil {
+		r.Done()
+		log.Printf("Push Error: %s", err)
+	}
+	log.Printf("Pushed: %s", target)
+}
 
 type ResponseBuffer struct {
 	body *bytes.Buffer
@@ -53,9 +93,11 @@ func (b *ResponseBuffer) ReadAll() ([]byte, error) {
 
 func (b *ResponseBuffer) Flush() {
 	io.Copy(b.w, bytes.NewReader(b.body.Bytes()))
+	b.body.Reset()
 }
 
-func processPushes(ops map[string]jq.Op, b []byte, p http.Pusher) {
+func getPushLinks(ops map[string]jq.Op, b []byte) []string {
+	var pushes []string
 	for part, op := range ops {
 		data, err := op.Apply(b)
 		if err != nil {
@@ -81,19 +123,11 @@ func processPushes(ops map[string]jq.Op, b []byte, p http.Pusher) {
 
 		for _, link := range links {
 			if url, err := url.Parse(link); err == nil {
-				push := url.Path + query
-				log.Printf("Pushing: %s", push)
-				opts := &http.PushOptions{
-					Header: http.Header{"X-Push": []string{"ON"}},
-				}
-				if err := p.Push(push, opts); err == nil {
-					log.Printf("Pushed: %s", push)
-				} else {
-					log.Printf("Push Error: %s", err)
-				}
+				pushes = append(pushes, url.RequestURI()+query)
 			}
 		}
 	}
+	return pushes
 }
 
 func main() {
@@ -105,50 +139,81 @@ func main() {
 		r.Header.Set("X-Forwarded-Proto", "https")
 		d(r)
 	}
-	http.Handle("/", http.FileServer(http.Dir(".")))
-	http.Handle("/jsonapi/", handler(backend))
+	http.Handle("/lib/", http.FileServer(http.Dir(".")))
+	http.Handle("/dclient.html", http.FileServer(http.Dir(".")))
+	http.Handle("/", handler(backend))
 	log.Fatalln(http.ListenAndServeTLS(":443", "./server.crt", "./server.key", nil))
 }
 
 func handler(backend *httputil.ReverseProxy) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if _, ok := w.(http.Pusher); !ok {
-			log.Println("HTTP/2 is not supported by the client.")
-		}
-		rb := NewResponseBuffer(w)
-		if r.Header.Get("X-Push") != "ON" {
+		isPush := r.Header.Get("X-Push") == "ON"
+		// Introduce some artificial latency
+		if !isPush {
 			time.Sleep(time.Millisecond * 150)
 		}
-		backend.ServeHTTP(rb, r)
-		p, ok := w.(http.Pusher)
-		if !ok {
-			log.Println("HTTP/2 is not supported")
+
+		if r.Header.Get("Accept") != "application/vnd.api+json" {
+			backend.ServeHTTP(w, r)
+			return
 		}
-		ops := parsePaths(r.Header["X-Push-Please"])
-		if len(ops) > 0 {
-			if bs, err := rb.ReadAll(); err == nil {
-				processPushes(ops, bs, p)
+
+		rb := NewResponseBuffer(w)
+		backend.ServeHTTP(rb, r)
+		defer rb.Flush()
+
+		requestID := r.Header.Get("X-Push-Request-ID")
+		if requestID == "" {
+			p, ok := w.(http.Pusher)
+			if !ok {
+				log.Println("HTTP/2 is not supported")
+			} else {
+				requestID = uuid.NewV4().String()
+				requestPushers.add(requestID, &RequestPusher{p: p})
+				defer requestPushers.remove(requestID)
 			}
 		}
-		rb.Flush()
+
+		if requestID != "" {
+			if pusher, ok := requestPushers.get(requestID); ok {
+				ops := parsePushPlease(r.Header.Get("X-Push-Please"))
+				if len(ops) > 0 {
+					if bs, err := rb.ReadAll(); err == nil {
+						if pushes := getPushLinks(ops, bs); len(pushes) > 0 {
+							headers := r.Header
+							headers.Set("X-Push", "ON")
+							headers.Set("X-Push-Request-ID", requestID)
+							opts := &http.PushOptions{Header: headers}
+							for _, target := range pushes {
+								pusher.Push(target, opts)
+							}
+						}
+					}
+				}
+
+				if isPush {
+					pusher.Done()
+				} else {
+					pusher.Wait()
+				}
+			}
+		}
 	})
 }
 
-func parsePaths(headers []string) map[string]jq.Op {
+func parsePushPlease(please string) map[string]jq.Op {
 	ops := make(map[string]jq.Op)
-	for _, headerValues := range headers {
-		for _, part := range strings.Split(headerValues, ";") {
-			// Invalid paths are simply ignored.
-			trimmed := strings.TrimSpace(part)
-			var path string
-			if strings.Contains(trimmed, "?") {
-				path = strings.Split(trimmed, "?")[0]
-			} else {
-				path = trimmed
-			}
-			if op, err := jq.Parse(path); err == nil {
-				ops[trimmed] = op
-			}
+	for _, part := range strings.Split(please, ";") {
+		// Invalid paths are simply ignored.
+		trimmed := strings.TrimSpace(part)
+		var path string
+		if strings.Contains(trimmed, "?") {
+			path = strings.Split(trimmed, "?")[0]
+		} else {
+			path = trimmed
+		}
+		if op, err := jq.Parse(path); err == nil {
+			ops[trimmed] = op
 		}
 	}
 	return ops
